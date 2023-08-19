6e81adde8dab75bf20ecc0661104709667aa388b
Luis Villalobos <57929788+solidwolf@users.noreply.github.com>
2023-08-19 15:24:15 -0500

    missed some stuff

--- app/src/main/java/solid/wolf/dangoapp/ui/theme/Spacing.kt
+++ app/src/main/java/solid/wolf/dangoapp/ui/theme/Spacing.kt
@@ -1,3 +1,17 @@
 package solid.wolf.dangoapp.ui.theme
 
-data class Spacing()
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object Spacing{
+    val spacing0: Dp = 0.dp
+    val spacing1: Dp = 1.dp
+    val spacing2: Dp = 2.dp
+    val spacing4: Dp = 4.dp
+    val spacing8: Dp = 8.dp
+    val spacing16: Dp = 16.dp
+    val spacing32: Dp = 32.dp
+    val spacing64: Dp = 64.dp
+    val spacing128: Dp = 128.dp
+    val spacing256: Dp = 256.dp
+}
